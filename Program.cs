f0efc844133fb4019071b7461c5bedf624f88902
Marius <mariusmadan23@gmail.com>
2024-12-05 13:24:51 +0100

    Main Program
    Customer Support System

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using CustomerSupportSystem.Handlers;  // For handler classes
+using CustomerSupportSystem.Models;    // For the Query class
+
+namespace CustomerSupportSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customerRep = new CustomerServiceRep();
+            var manager = new Manager();
+            var seniorManager = new SeniorManager();
+
+            customerRep.SetNext(manager).SetNext(seniorManager);
+
+            var queries = new[]
+            {
+                new Query(Query.ComplexityLevel.Basic, "Password reset request"),
+                new Query(Query.ComplexityLevel.Intermediate, "Billing discrepancy"),
+                new Query(Query.ComplexityLevel.Advanced, "System outage"),
+            };
+
+            Console.WriteLine("Handling Queries:");
+            foreach (var query in queries)
+            {
+                customerRep.HandleRequest(query);
+            }
+                Console.WriteLine();
+
+            Console.WriteLine("Press Enter to exit...");
+            Console.ReadLine();
+        }
+    }
+}
